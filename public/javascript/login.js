fc7a7075dc49b2d52bfbde8e6c8fa612464433c6
Cory Calaway <cory.c.calaway@gmail.com>
2021-02-26 14:51:12 -0700

    added event listener for creating profile

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,1 +1,41 @@
 //const test
+
+
+// const { JSDOM } = require( "jsdom" );
+// const { window } = new JSDOM( "" );
+// const $ = require( "jquery" )( window );
+
+async function userCreate(test) {
+    console.log('pass')
+    console.log(test)
+    // event.preventDefault();
+  
+    // const email = document.querySelector('#email-login').value.trim();
+    // const password = document.querySelector('#password-login').value.trim();
+  
+    // if (email && password) {
+    //   const response = await fetch('/api/users/login', {
+    //     method: 'post',
+    //     body: JSON.stringify({
+    //       email,
+    //       password
+    //     }),
+    //     headers: { 'Content-Type': 'application/json' }
+    //   });
+  
+    //   if (response.ok) {
+    //     document.location.replace('/');
+    //   } else {
+    //     alert(response.statusText);
+    //   }
+    // }
+}
+
+// document.querySelector('.user-form').addEventListener('submit', userCreate);
+// $(".user-form").on('submit', userCreate)
+
+// $(".user-form").click(function(){
+//     console.log('pass')
+// })
+
+$(".user-form").click('submit', userCreate)
