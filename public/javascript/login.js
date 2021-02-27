561445f835c4bcc09b0a0fc60477d26efc63cd72
Cory Calaway <cory.c.calaway@gmail.com>
2021-02-26 20:12:17 -0700

    all data from usercreate modal console logged upon submit

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,12 +2,27 @@
 
 
 // const { JSDOM } = require( "jsdom" );
-// const { window } = new JSDOM( "" );
+// const { window} = new JSDOM( "" );
 // const $ = require( "jquery" )( window );
 
-async function userCreate(test) {
+async function userCreate(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-data').value.trim();
+    const password = document.querySelector('#password-data').value.trim();
+    const underSeven = document.querySelector('#underSeven-data').value.trim();
+    const overSeven = document.querySelector('#overSeven-data').value.trim();
+    const weeksPrep = document.querySelector('#weeksPrep-data').value.trim();
+    const diet = document.querySelector('#diet-data').value.trim();
     console.log('pass')
-    console.log(test)
+    console.log(username)
+    console.log(email)
+    console.log(password)
+    console.log(underSeven)
+    console.log(overSeven)
+    console.log(weeksPrep)
+    console.log(diet)
     // event.preventDefault();
   
     // const email = document.querySelector('#email-login').value.trim();
@@ -30,12 +45,13 @@ async function userCreate(test) {
     //   }
     // }
 }
+// console.log(document.querySelector('.user-form'))
+document.querySelector('.user-form').addEventListener('submit', userCreate);
 
-// document.querySelector('.user-form').addEventListener('submit', userCreate);
 // $(".user-form").on('submit', userCreate)
 
 // $(".user-form").click(function(){
 //     console.log('pass')
 // })
 
-$(".user-form").click('submit', userCreate)
+// $(".user-form").click('submit', userCreate)
