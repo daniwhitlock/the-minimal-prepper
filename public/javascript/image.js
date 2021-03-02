751fbc506cc6bd96455398f53a31c1522a3f7d7f
Cory Calaway <cory.c.calaway@gmail.com>
2021-03-01 23:52:09 -0700

    added event listener for image click

---
+++ public/javascript/image.js
@@ -0,0 +1,8 @@
+async function uploadImage(event) {
+    event.preventDefault();
+  
+    console.log('win')
+  }
+
+ 
+  document.getElementById("image-save").addEventListener("click", uploadImage);
