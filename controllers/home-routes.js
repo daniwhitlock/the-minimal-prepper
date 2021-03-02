774a292de1bafc25c3f7e69e1afbea1bb7362081
Fire Dragon <daniwhitlock4@gmail.com>
2021-03-01 21:58:29 -0700

    cleaning up code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,39 +3,7 @@ const sequelize = require('../config/connection');
 
 router.get('/', (req, res) => {
     console.log(req.session);
-    res.render('homepage', {req})
-    // Post.findAll({
-    //   attributes: [
-    //     'id',
-    //     'post_url',
-    //     'title',
-    //     'created_at',
-    //     [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    //   ],
-    //   include: [
-    //     {
-    //       model: Comment,
-    //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-    //       include: {
-    //         model: User,
-    //         attributes: ['username']
-    //       }
-    //     },
-    //     {
-    //       model: User,
-    //       attributes: ['username']
-    //     }
-    //   ]
-    // })
-    //   .then(dbPostData => {
-    //     // pass a single post object into the homepage template
-    //     const posts = dbPostData.map(post => post.get({ plain: true }));
-    //     res.render('homepage', { posts });
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //     res.status(500).json(err);
-    //   });
+    res.render('homepage', {req});
   });
 
   router.get('/profile', (req, res) => {
