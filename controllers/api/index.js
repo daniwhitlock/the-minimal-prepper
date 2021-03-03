09c71ef3db58b3a0a41db09dc775970e674024ee
Steven Baty <baty_steven@yahoo.com>
2021-03-02 20:41:53 -0700

    created Comment.js, utils/auth.js, comment-routes.js, add .env, installed express and mysql database

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 // const postRoutes = require('./post-routes');
-// const commentRoutes = require('./comment-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 // router.use('/posts', postRoutes);
