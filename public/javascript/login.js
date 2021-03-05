b150a1a4468c07d4aa4bc88333595f0b34695bac
Cory Calaway <cory.c.calaway@gmail.com>
2021-03-05 11:45:34 -0700

    fixes and clean up

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,72 +1,47 @@
-// const { JSDOM } = require( "jsdom" );
-// const { window} = new JSDOM( "" );
-// const $ = require( "jquery" )( window );
-
 async function userCreate(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-data').value.trim();
-    const password = document.querySelector('#password-data').value.trim();
-    const underseven = document.querySelector('#underSeven-data').value.trim();
-    const overSeven = document.querySelector('#overSeven-data').value.trim();
-    const weeksPrep = document.querySelector('#weeksPrep-data').value.trim();
-    // const diet = document.querySelector('#diet-data').value.trim();
-    console.log('pass');
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-data').value.trim();
+  const password = document.querySelector('#password-data').value.trim();
+  const underseven = document.querySelector('#underSeven-data').value.trim();
+  const overSeven = document.querySelector('#overSeven-data').value.trim();
+  const weeksPrep = document.querySelector('#weeksPrep-data').value.trim();
+  // const diet = document.querySelector('#diet-data').value.trim();
+  console.log('pass');
+  console.log(username);
+  console.log(email);
+  console.log(password);
+  console.log(underseven);
+  console.log(overSeven);
+  console.log(weeksPrep);
+  // console.log(diet);
+  if (username) {
+
     console.log(username);
-    console.log(email);
-    console.log(password);
-    console.log(underseven);
-    console.log(overSeven);
-    console.log(weeksPrep);
-    // console.log(diet);
-    if (username) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+        underseven,
+        overSeven,
+        weeksPrep,
+        // diet
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    console.log(username);
+    // check the response status
+    if (response.ok) {
+      location.reload();
+    } else {
+      console.log('dang')
+      alert(response.statusText);
+    }
+  }
 
-        console.log(username);
-        const response = await fetch('/api/users', {
-          method: 'post',
-          body: JSON.stringify({
-            username,
-            email,
-            password,
-            underseven,
-            overSeven,
-            weeksPrep,
-            // diet
-          }),
-          headers: { 'Content-Type': 'application/json' }
-        });
-        console.log(username);
-        // check the response status
-        if (response.ok) {
-          console.log('success');
-          location.reload();
-        } else {
-            console.log('dang')
-          alert(response.statusText);
-        }
-      }
-    // event.preventDefault();
-  
-    // const email = document.querySelector('#email-login').value.trim();
-    // const password = document.querySelector('#password-login').value.trim();
-  
-    // if (email && password) {
-    //   const response = await fetch('/api/users/login', {
-    //     method: 'post',
-    //     body: JSON.stringify({
-    //       email,
-    //       password
-    //     }),
-    //     headers: { 'Content-Type': 'application/json' }
-    //   });
-  
-    //   if (response.ok) {
-    //     document.location.replace('/');
-    //   } else {
-    //     alert(response.statusText);
-    //   }
-    // }
 }
 // console.log(document.querySelector('.user-form'));
 
@@ -77,20 +52,20 @@ async function loginForm(event) {
   const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
-      const response = await fetch('/api/users/login', {
-          method: 'post',
-          body: JSON.stringify({
-              username,
-              password
-          }),
-          headers: { 'Content-Type': 'application/json' }
-      });
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
 
-      if (response.ok) {
-         document.location.replace('/profile');
-      } else {
-          alert(response.statusText);
-      }
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
   }
 }
 
