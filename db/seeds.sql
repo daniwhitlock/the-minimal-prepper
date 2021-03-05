0530b148f19479d8873dbae1bf9ffcf14bc094c7
Fire Dragon <daniwhitlock4@gmail.com>
2021-03-04 17:34:18 -0700

    added comments on pantry generator and 72 hour kit

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,4 +5,3 @@ VALUES
     ('Header', 'Title', 'www.exampleurl.com', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sedreiciendis corporis laudantium hicofficiis error voluptas deleniti cumque alias assumenda expedita commodi, aliquidiusto ab, isteeligendi consectetur maxime odit', '03/03/21', '03/03/21'),
     ('Header', 'Title', 'www.exampleurl.com', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sedreiciendis corporis laudantium hicofficiis error voluptas deleniti cumque alias assumenda expedita commodi, aliquidiusto ab, isteeligendi consectetur maxime odit', '03/03/21', '03/03/21');
     
-    
