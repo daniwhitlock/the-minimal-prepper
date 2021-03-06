fdf262c8b13b121a42b1bbf1b9b484b710b026da
Cory Calaway <cory.c.calaway@gmail.com>
2021-03-06 07:48:39 -0700

    heroku changes

--- models/index.js
+++ models/index.js
@@ -1,14 +1,6 @@
 // const Posts = require('./Posts');
 const User = require('./User');
-<<<<<<< HEAD
 
 
 
 module.exports = { User };
-=======
-const Articles = require('./Articles');
-
-
-
-module.exports = { User, Articles };
->>>>>>> d611c6596fed9785cd8bfb6c2c67bf8a88d4390f
